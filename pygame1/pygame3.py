b0b0b98045fe8feabee3eae57086926232557296
OldEase <tarasov.kd@phystech.edu>
2021-10-05 10:51:05 +0300

    Update pygame3.py
    renaming and format changing

--- pygame1/pygame3.py
+++ pygame1/pygame3.py
@@ -6,82 +6,238 @@ FPS = 30
 screen = pygame.display.set_mode((400, 600))
 
 
-def ghost(a, b, c, d, side):  # призрак side выбирает направление взгляда :1 налево, 0 направо
-    i = c / 6
-    j = d / 10
-    place = pygame.Surface((c, d), pygame.SRCALPHA)  # создаем полупрозрачную область для каждого призрака
-    pygame.draw.ellipse(place, (0, 200, 100, 160), (i, 0, 4 * i, 6 * j))  # голова
-    pygame.draw.polygon(place, (0, 200, 100, 160),
-                        [[i, 3 * j], [0, 9 * j], [i, 8 * j],
-                         [2 * i, 10 * j], [2 * i, 7 * j], [3 * i, 9 * j], [3 * i, 8 * j], [4 * i, 10 * j],
-                         [4 * i, 8 * j], [5 * i, 9 * j], [5 * i, 8 * j], [6 * i, 10 * j], [5 * i, 3 * j]])  # тело
+def ghost(x_coordinate, y_coordinate, start_proportion_x, start_proportion_y, side, color_body):  # призрак side выбирает направление взгляда :1 налево, 0 направо
+
+    proportion_x = start_proportion_x / 6
+    proportion_y = start_proportion_y / 10
+    place = pygame.Surface((start_proportion_x, start_proportion_y), pygame.SRCALPHA)  # создаем полупрозрачную область для каждого призрака
+    pygame.draw.ellipse(
+                        place,
+                        color_body,
+                        (
+                         proportion_x, 0,
+                         4 * proportion_x, 6 * proportion_y
+                        )
+    )  # голова
+    pygame.draw.polygon(
+                        place,
+                        color_body,
+                        [
+                         (proportion_x, 3 * proportion_y),
+                         (0, 9 * proportion_y),
+                         (proportion_x, 8 * proportion_y),
+                         (2 * proportion_x, 10 * proportion_y),
+                         (2 * proportion_x, 7 * proportion_y),
+                         (3 * proportion_x, 9 * proportion_y),
+                         (3 * proportion_x, 8 * proportion_y),
+                         (4 * proportion_x, 10 * proportion_y),
+                         (4 * proportion_x, 8 * proportion_y),
+                         (5 * proportion_x, 9 * proportion_y),
+                         (5 * proportion_x, 8 * proportion_y),
+                         (6 * proportion_x, 10 * proportion_y),
+                         (5 * proportion_x, 3 * proportion_y)
+                        ]
+    )  # тело
     if side == 1:  # лицо если смотрит налево
-        pygame.draw.ellipse(place, (230, 0, 0, 200), (7 * i / 4, 2 * j, i / 2, j))
-        pygame.draw.ellipse(place, (0, 0, 0, 200), (7 * i / 4, 2 * j, i / 2, j), 2)
-        pygame.draw.ellipse(place, (230, 0, 0, 200), (11 * i / 4, 2 * j, i / 2, j))
-        pygame.draw.ellipse(place, (0, 0, 0, 200), (11 * i / 4, 2 * j, i / 2, j), 2)
-        pygame.draw.lines(place, (0, 0, 0, 200), False,
-                          [[2 * i, 7 * j / 2], [2.25 * i, 4 * j], [2.5 * i, 3.75 * j], [2.75 * i, 4 * j],
-                           [3 * i, 3.5 * j]], 2)
+        pygame.draw.ellipse(
+                            place,
+                            (230, 0, 0, 200),
+                            (
+                             7 * proportion_x / 4, 2 * proportion_y,
+                             proportion_x / 2, proportion_y
+                             )
+        )
+        pygame.draw.ellipse(
+                            place,
+                            (0, 0, 0, 200),
+                            (
+                             7 * proportion_x / 4, 2 * proportion_y,
+                             proportion_x / 2, proportion_y
+                             ),
+                            2
+        )
+        pygame.draw.ellipse(
+                            place,
+                            (230, 0, 0, 200),
+                            (
+                             11 * proportion_x / 4, 2 * proportion_y,
+                             proportion_x / 2, proportion_y
+                             )
+        )
+        pygame.draw.ellipse(
+                            place,
+                            (0, 0, 0, 200),
+                            (
+                             11 * proportion_x / 4, 2 * proportion_y,
+                             proportion_x / 2, proportion_y
+                             ),
+                            2
+        )
+        pygame.draw.lines(
+                          place,
+                          (0, 0, 0, 200),
+                          False,
+                          [
+                           [2 * proportion_x, 7 * proportion_y / 2],
+                           [2.25 * proportion_x, 4 * proportion_y],
+                           [2.5 * proportion_x, 3.75 * proportion_y],
+                           [2.75 * proportion_x, 4 * proportion_y],
+                           [3 * proportion_x, 3.5 * proportion_y]
+                          ],
+                           2
+        )
     elif side == 0:  # лицо если смотрит направо
-        pygame.draw.ellipse(place, (230, 0, 0, 200), (i + 7 * i / 4, 2 * j, i / 2, j))
-        pygame.draw.ellipse(place, (0, 0, 0, 200), (i + 7 * i / 4, 2 * j, i / 2, j), 2)
-        pygame.draw.ellipse(place, (230, 0, 0, 200), (i + 11 * i / 4, 2 * j, i / 2, j))
-        pygame.draw.ellipse(place, (0, 0, 0, 200), (i + 11 * i / 4, 2 * j, i / 2, j), 2)
-        pygame.draw.lines(place, (0, 0, 0, 200), False,
-                          [[i + 2 * i, 7 * j / 2], [i + 2.25 * i, 4 * j], [i + 2.5 * i, 3.75 * j],
-                           [i + 2.75 * i, 4 * j],
-                           [i + 3 * i, 3.5 * j]], 2)
+        pygame.draw.ellipse(
+                            place,
+                            (230, 0, 0, 200),
+                            (
+                            proportion_x + 7 * proportion_x / 4, 2 * proportion_y,
+                            proportion_x / 2, proportion_y
+                            )
+        )
+        pygame.draw.ellipse(
+                            place,
+                            (0, 0, 0, 200),
+                            (
+                             proportion_x + 7 * proportion_x / 4,
+                             2 * proportion_y, proportion_x / 2,
+                             proportion_y
+                            ),
+                            2
+        )
+        pygame.draw.ellipse(
+                            place,
+                            (230, 0, 0, 200),
+                            (
+                             proportion_x + 11 * proportion_x / 4,
+                             2 * proportion_y, proportion_x / 2,
+                             proportion_y
+                            )
+        )
+        pygame.draw.ellipse(
+                            place,
+                            (0, 0, 0, 200),
+                            (
+                             proportion_x + 11 * proportion_x / 4,
+                             2 * proportion_y, proportion_x / 2,
+                             proportion_y
+                            ),
+                            2
+        )
+        pygame.draw.lines(
+                          place,
+                          (0, 0, 0, 200),
+                          False,
+                          [
+                           [proportion_x + 2 * proportion_x, 7 * proportion_y / 2],
+                           [proportion_x + 2.25 * proportion_x, 4 * proportion_y],
+                           [proportion_x + 2.5 * proportion_x, 3.75 * proportion_y],
+                           [proportion_x + 2.75 * proportion_x, 4 * proportion_y],
+                           [proportion_x + 3 * proportion_x, 3.5 * proportion_y]
+                          ],
+                          2
+        )
 
-    screen.blit(place, (a, b))
+    screen.blit(place, (x_coordinate, y_coordinate))
 
 
-def cloud(a, b, c, d, e, f, g, ):  # облако
-    pygame.draw.ellipse(screen, (a, b, c), (d, e, f, g))
+def cloud(color, x_start, y_start, x_size, y_size):  # облако
+    pygame.draw.ellipse(screen, color, (x_start, y_start, x_size, y_size))
 
 
-def house(a, b, c, d):  # отрисовка дома
-    pygame.draw.rect(screen, (70, 0, 0), (a, b, c, d), 0)  # корпус дома
-    pygame.draw.rect(screen, (0, 0, 50), ((a + c / 13), (b + 3 * d / 7), ((3 * c) / 13), (d / 3)),
-                     0)  # левое нижнее окно
-    pygame.draw.rect(screen, (0, 0, 50), ((a + (5 * c) / 13), (b + 3 * d / 7), ((3 * c) / 13), (d / 3)),
-                     0)  # среднее нижнее окно
-    pygame.draw.rect(screen, (250, 240, 0), ((a + (9 * c) / 13), (b + 3 * d / 7), ((3 * c) / 13), (d / 3)),
-                     0)  # правое нижнее окно
-    pygame.draw.line(screen, (70, 0, 0), ((a + c / 13), (b + 3 * d / 7 + d / 6)),
-                     ((a + 12 * c / 13), (b + 3 * d / 7 + d / 6)), 3)  # горизонтальный импост для нижних окон
+def house(x_start, y_start, x_size, y_size):  # отрисовка дома
+    pygame.draw.rect(
+                     screen,
+                     (70, 0, 0),
+                     (
+                      x_start, y_start,
+                      x_size, y_size
+                     ),
+                     0
+    )  # корпус дома
+    pygame.draw.rect(
+                     screen,
+                     (0, 0, 50),
+                     (
+                      (x_start + x_size / 13), (y_start + 3 * y_size / 7),
+                      ((3 * x_size) / 13), (y_size / 3)
+                     ),
+                     0
+    )  # левое нижнее окно
+    pygame.draw.rect(
+                     screen,
+                     (0, 0, 50),
+                     (
+                      (x_start + 5 * x_size / 13), (y_start + 3 * y_size / 7),
+                      (3 * x_size / 13), (y_size / 3)
+                     ),
+                     0
+    )  # среднее нижнее окно
+    pygame.draw.rect(
+                     screen,
+                     (250, 240, 0),
+                     (
+                      (x_start + 9 * x_size / 13), (y_start + 3 * y_size / 7),
+                      (3 * x_size / 13), (y_size / 3)
+                     ),
+                     0
+    )  # правое нижнее окно
+    pygame.draw.line(
+                     screen,
+                     (70, 0, 0),
+                     (
+                      (x_start + x_size / 13),
+                      (y_start + 3 * y_size / 7 + y_size / 6)
+                     ),
+                     (
+                      (x_start + 12 * x_size / 13),
+                      (y_start + 3 * y_size / 7 + y_size / 6)
+                     ),
+                     3
+    )  # горизонтальный импост для нижних окон
     for i in range(3):  # вертикальные импосты
-        pygame.draw.line(screen, (70, 0, 0), ((a + (5 + 8 * i) * c / 26), (b + 3 * d / 7)),
-                         ((a + (5 + 8 * i) * c / 26), (b + d / 3 + 3 * d / 7)), 3)  #
+        pygame.draw.line(
+                         screen,
+                         (70, 0, 0),
+                         (
+                          (x_start + (5 + 8 * i) * x_size / 26),
+                          (y_start + 3 * y_size / 7)
+                         ),
+                         (
+                          (x_start + (5 + 8 * i) * x_size / 26),
+                          (y_start + y_size / 3 + 3 * y_size / 7)
+                         ),
+                         3
+        )  #
 
-    pygame.draw.rect(screen, (250, 240, 0), ((a + c / 5), (b + d / 20), (3 * c / 5), (5 * d / 30)), 0)  # верхнее окно
-    pygame.draw.rect(screen, (0, 0, 0), ((a + c / 10), (b + d / 10), (4 * c / 5), (d / 6)), 3)  # кортур балкончика
+    pygame.draw.rect(screen, (250, 240, 0), ((x_start + x_size / 5), (y_start + y_size / 20), (3 * x_size / 5), (5 * y_size / 30)), 0)  # верхнее окно
+    pygame.draw.rect(screen, (0, 0, 0), ((x_start + x_size / 10), (y_start + y_size / 10), (4 * x_size / 5), (y_size / 6)), 3)  # кортур балкончика
     for i in range(10):  # перекладины балкончика
-        pygame.draw.line(screen, (0, 0, 0), ((a + c / 10 + i * (4 * c / 5) / 10), (b + d / 10)),
-                         ((a + c / 10 + i * (4 * c / 5) / 10), (d / 6 + b + d / 10)), 3)
+        pygame.draw.line(screen, (0, 0, 0), ((x_start + x_size / 10 + i * (4 * x_size / 5) / 10), (y_start + y_size / 10)),
+                         ((x_start + x_size / 10 + i * (4 * x_size / 5) / 10), (y_size / 6 + y_start + y_size / 10)), 3)
     pygame.draw.polygon(screen, (40, 0, 0),
-                        [[a - c / 10, b], [c + a + c / 10, b], [c + a - c / 10, b - d / 10],
-                         [a + c / 10, b - d / 10]])  # крыша
-    pygame.draw.rect(screen, (40, 0, 0), ((a + c / 5), (b - d / 5), (c / 10), (d / 10)), 0)  # труба 1
-    pygame.draw.rect(screen, (40, 0, 0), ((a + 2 * c / 5), (b - d / 5), (c / 10), (d / 10)), 0)  # труба 2
+                        [[x_start - x_size / 10, y_start], [x_size + x_start + x_size / 10, y_start], [x_size + x_start - x_size / 10, y_start - y_size / 10],
+                         [x_start + x_size / 10, y_start - y_size / 10]])  # крыша
+    pygame.draw.rect(screen, (40, 0, 0), ((x_start + x_size / 5), (y_start - y_size / 5), (x_size / 10), (y_size / 10)), 0)  # труба 1
+    pygame.draw.rect(screen, (40, 0, 0), ((x_start + 2 * x_size / 5), (y_start - y_size / 5), (x_size / 10), (y_size / 10)), 0)  # труба 2
 
 
 pygame.draw.rect(screen, (130, 130, 130), (0, 0, 400, 250), 0)  # небо
 pygame.draw.circle(screen, (255, 255, 255), (350, 50), 35, 0)  # луна
-cloud(50, 50, 50, 100, 50, 300, 60)
-cloud(40, 40, 40, 50, 100, 250, 50)
-cloud(80, 80, 80, 150, 20, 150, 50)
-cloud(70, 70, 70, 150, 150, 250, 50)
-cloud(40, 40, 40, 20, 170, 180, 50)
+cloud((50, 50, 50), 100, 50, 300, 60)
+cloud((40, 40, 40), 50, 100, 250, 50)
+cloud((80, 80, 80), 150, 20, 150, 50)
+cloud((70, 70, 70), 150, 150, 250, 50)
+cloud((40, 40, 40), 20, 170, 180, 50)
 house(140, 150, 100, 140)
 house(10, 300, 170, 220)
 house(310, 180, 120, 160)
-ghost(30, 430, 90, 140, 0)
-ghost(190, 260, 80, 120, 0)
-ghost(150, 300, 80, 120, 1)
-ghost(250, 200, 40, 70, 1)
-ghost(100, 150, 40, 70, 0)
-ghost(200, 300, 400, 600, 1)
+ghost(30, 430, 90, 140, 0, (255, 255, 255, 160))
+ghost(190, 260, 80, 120, 0, (0, 200, 100, 160))
+ghost(150, 300, 80, 120, 1, (0, 200, 100, 160))
+ghost(250, 200, 40, 70, 1, (0, 200, 100, 160))
+ghost(100, 150, 40, 70, 0, (0, 200, 100, 160))
+ghost(200, 300, 400, 600, 1, (0, 200, 100, 160))
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
