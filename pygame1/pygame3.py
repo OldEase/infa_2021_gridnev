b1c10b8ddd21c1ce22835ea375047f1dfad17a37
OldEase <tarasov.kd@phystech.edu>
2021-10-05 22:21:07 +0300

    finally, description added

--- pygame1/pygame3.py
+++ pygame1/pygame3.py
@@ -10,10 +10,14 @@ def ghost_head(
         screen_for_place,
         proportion_x, proportion_y,
         color
-        ):
+):
     '''
-    Function for drawing ghost's head
-    screen_for_place - surface for '''
+    Function for drawing ghost's head\n
+    screen_for_place - surface for drawing\n
+    proportion_x - size of x\n
+    proportion_y - size of y\n
+    color - color of head\n
+    '''
     pygame.draw.ellipse(
         screen_for_place,
         color,
@@ -25,10 +29,18 @@ def ghost_head(
 
 
 def ghost_body(
+
         place,
         color,
         proportion_x, proportion_y
-        ):
+):
+    '''
+    Function for drawing ghost's body\n
+    place - surface for drawing\n
+    proportion_x - size of body in x\n
+    proportion_y - size of body in y\n
+    color - color of body\n
+    '''
     pygame.draw.polygon(
         place,
         color,
@@ -55,7 +67,16 @@ def ghost_orient(
         proportion_x, proportion_y,
         color_eye,
         side
-        ):
+):
+    '''
+    Function for orientation of ghost and drawing face\n
+    place - surface for drawing\n
+    proportion_x - size of face in x\n
+    proportion_y - size of face in y\n
+    color_eye - color of eyes\n
+    side - if side == 1 face looking at left\n
+    if side == 0 face looking at right\n
+    '''
     if side == 1:  # лицо если смотрит налево
         pygame.draw.ellipse(
             place,
@@ -164,8 +185,18 @@ def ghost(
         side,
         color_body,
         color_eye
-        ):  # призрак side выбирает направление взгляда :1 налево, 0 направо
-
+):  # призрак side выбирает направление взгляда :1 налево, 0 направо
+    '''
+    Function for drawing ghost \n
+    screen_for_place - surface for drawing \n
+    x_coordinate, y_coordinate - coordinates of upper left angle \n
+    proportion_x - size of ghost in x \n
+    proportion_y - size of ghost in y \n
+    side - if side == 1 face looking at left \n
+    if side == 0 face looking at right \n
+    color_body - color of body \n
+    color_eye - color of eyes \n
+    '''
     proportion_x = start_proportion_x / 6
     proportion_y = start_proportion_y / 10
 
@@ -198,10 +229,19 @@ def ghost(
 
 
 def cloud(
+        screen,
         color,
         x_start, y_start,
         x_size, y_size
-        ):  # облако
+):  # облако
+    '''
+    Function for drawing cloud\n
+    screen - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of cloud in x\n
+    y_size - size of cloud in y\n
+    color - color of cloud\n
+    '''
     pygame.draw.ellipse(
         screen,
         color,
@@ -217,7 +257,15 @@ def house_body(
         x_start, y_start,
         x_size, y_size,
         color_body
-        ):
+):
+    '''
+    Function for drawing house's body\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of cloud in x\n
+    y_size - size of cloud in y\n
+    color_body - color of house's body\n
+    '''
     pygame.draw.rect(
         screen_for_place,
         color_body,
@@ -233,7 +281,15 @@ def house_window_left(
         x_start, y_start,
         x_size, y_size,
         color_window_left
-        ):
+):
+    '''
+    Function for drawing left window\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of window in x\n
+    y_size - size of window in y\n
+    color_window_left - color of window\n
+    '''
     pygame.draw.rect(
         screen_for_place,
         color_window_left,
@@ -249,7 +305,15 @@ def house_window_middle(
         x_start, y_start,
         x_size, y_size,
         color_window_middle
-        ):
+):
+    '''
+    Function for drawing middle window\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of window in x\n
+    y_size - size of window in y\n
+    color_window_middle - color of window\n
+    '''
     pygame.draw.rect(
         screen_for_place,
         color_window_middle,
@@ -265,7 +329,15 @@ def house_window_right(
         x_start, y_start,
         x_size, y_size,
         color_window_right
-        ):
+):
+    '''
+    Function for drawing right window\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of window in x\n
+    y_size - size of window in y\n
+    color_window_right - color of window\n
+    '''
     pygame.draw.rect(
         screen_for_place,
         color_window_right,
@@ -281,7 +353,15 @@ def house_horizontal_impost(
         x_start, y_start,
         x_size, y_size,
         color
-        ):
+):
+    '''
+    Function for drawing horizontal impost\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of impost in x\n
+    y_size - size of impost in y\n
+    color - color of impost\n
+    '''
     pygame.draw.line(
         screen_for_place,
         color,
@@ -302,7 +382,15 @@ def house_vertical_impost(
         x_start, y_start,
         x_size, y_size,
         color
-        ):
+):
+    '''
+    Function for drawing vertical impost\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of impost in x\n
+    y_size - size of impost in y\n
+    color - color of impost\n
+    '''
     for i in range(3):  # вертикальные импосты
         pygame.draw.line(
             screen_for_place,
@@ -324,7 +412,15 @@ def house_impost(
         x_start, y_start,
         x_size, y_size,
         color
-        ):
+):
+    '''
+    Function for drawing impost\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of impost in x\n
+    y_size - size of impost in y\n
+    color - color of impost\n
+    '''
     house_horizontal_impost(screen_for_place, x_start, y_start, x_size, y_size, color)
     # горизонтальный импост для нижних окон
     house_vertical_impost(screen_for_place, x_start, y_start, x_size, y_size, color)
@@ -336,7 +432,15 @@ def house_balcony_window(
         x_start, y_start,
         x_size, y_size,
         color
-        ):
+):
+    '''
+    Function for drawing balcony window\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of balcony window in x\n
+    y_size - size of balcony window in y\n
+    color - color of window\n
+    '''
     pygame.draw.rect(
         screen_for_place,
         color,
@@ -352,7 +456,15 @@ def house_balcony_circuit(
         x_start, y_start,
         x_size, y_size,
         color
-        ):
+):
+    '''
+    Function for drawing balcony circuit\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of balcony circuit in x\n
+    y_size - size of balcony circuit in y\n
+    color - color of balcony circuit\n
+    '''
     pygame.draw.rect(
         screen_for_place,
         color,
@@ -369,7 +481,15 @@ def house_balcony_lattice(
         x_start, y_start,
         x_size, y_size,
         color
-        ):
+):
+    '''
+    Function for drawing balcony lattice\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of balcony lattice in x\n
+    y_size - size of balcony lattice in y\n
+    color - color of balcony lattice\n
+    '''
     for i in range(10):  # перекладины балкончика
         pygame.draw.line(
             screen_for_place,
@@ -391,7 +511,15 @@ def house_balcony(
         x_start, y_start,
         x_size, y_size,
         color
-        ):
+):
+    '''
+    Function for drawing balcony\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of balcony in x\n
+    y_size - size of balcony in y\n
+    color - color of balcony\n
+    '''
     house_balcony_circuit(screen_for_place, x_start, y_start, x_size, y_size, color)
 
     house_balcony_lattice(screen_for_place, x_start, y_start, x_size, y_size, color)
@@ -402,7 +530,15 @@ def house_roof(
         x_start, y_start,
         x_size, y_size,
         color
-        ):
+):
+    '''
+    Function for drawing roof\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of roof in x\n
+    y_size - size of roof in y\n
+    color - color of roof\n
+    '''
     pygame.draw.polygon(
         screen_for_place,
         color,
@@ -420,7 +556,15 @@ def house_pipe_left(
         x_start, y_start,
         x_size, y_size,
         color
-        ):
+):
+    '''
+    Function for drawing left pipe\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of left pipe in x\n
+    y_size - size of left pipe in y\n
+    color - color of left pipe\n
+    '''
     pygame.draw.rect(
         screen_for_place,
         color,
@@ -436,7 +580,15 @@ def house_pipe_right(
         x_start, y_start,
         x_size, y_size,
         color
-        ):
+):
+    '''
+    Function for drawing left pipe\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of left pipe in x\n
+    y_size - size of left pipe in y\n
+    color - color of left pipe\n
+    '''
     pygame.draw.rect(
         screen_for_place,
         color,
@@ -452,7 +604,15 @@ def house_pipes(
         x_start, y_start,
         x_size, y_size,
         color
-        ):
+):
+    '''
+    Function for drawing pipes\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of pipes in x\n
+    y_size - size of pipes in y\n
+    color - color of pipes\n
+    '''
     house_pipe_right(screen_for_place, x_start, y_start, x_size, y_size, color)
     house_pipe_left(screen_for_place, x_start, y_start, x_size, y_size, color)
 
@@ -464,7 +624,21 @@ def house(
         color_body, color_window_left,
         color_window_middle, color_window_right,
         color_window_up, color_balcony, color_roof, color_pipe
-        ):  # отрисовка дома
+):  # отрисовка дома
+    '''
+    Function for drawing house\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of house in x\n
+    y_size - size of house in y\n
+    color_body - color of body\n
+    color_window_left - color of window left\n
+    color_window_middle - color of window middle\n
+    color_window_up - color of balcony's window\n
+    color_balcony - color of balcony\n
+    color_roof - color of roof\n
+    color_pipe - color of pipe
+    '''
     house_body(
         screen_for_place,
         x_start, y_start,
@@ -534,6 +708,14 @@ def sky(screen_for_place,
         x_size, y_size,
         color
         ):
+    '''
+    Function for drawing house\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    x_size - size of house in x\n
+    y_size - size of house in y\n
+    color - color of sky
+    '''
     pygame.draw.rect(
         screen_for_place,
         color,
@@ -547,6 +729,13 @@ def moon(screen_for_place,
          radius,
          color
          ):
+    '''
+    Function for drawing house\n
+    screen_for_place - surface for drawing\n
+    x_start, y_start - coordinates of upper left angle\n
+    radius - radius of moon\n
+    color - color of sky
+    '''
     pygame.draw.circle(
         screen_for_place,
         color,
@@ -563,31 +752,36 @@ sky(
 )
 moon(
     screen,
-    350, 50, 
+    350, 50,
     35,
     (255, 255, 255)
 )
 cloud(
+    screen,
     (50, 50, 50),
     100, 50,
     300, 60
 )
 cloud(
+    screen,
     (40, 40, 40),
     50, 100,
     250, 50
 )
 cloud(
+    screen,
     (80, 80, 80),
     150, 20,
     150, 50
 )
 cloud(
+    screen,
     (70, 70, 70),
     150, 150,
     250, 50
 )
 cloud(
+    screen,
     (40, 40, 40),
     20, 170,
     180, 50
